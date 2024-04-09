3f15be4b0800734332e3f33206bca36845ceb9e8
Taras Savchyn <savik1992@gmail.com>
2024-04-10 00:08:57 +0300

    set settings/databases/dependencies

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from settings import settings
+
+SQLALCHEMY_DATABASE_URI = settings.DATABASE_URL
+
+engine = create_engine(
+    settings.SQLALCHEMY_DATABASE_URI,
+    connect_args="check_same_thread=False"
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
