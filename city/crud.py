6baeed3898e97059e800c8113ff80577a5fd2fb2
Taras Savchyn <savik1992@gmail.com>
2024-04-23 20:40:38 +0300

    add city endpints

--- city/crud.py
+++ city/crud.py
@@ -1,51 +1,19 @@
-from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from city.models import CityDB
-from city.schemas import City
-from dependencies import get_db
-from main import app
+from city.schemas import CityCreate
 
 
-@app.post("/cities/", response_model=City)
-def create_city(city: City, db: Session = Depends(get_db)):
-    db_city = CityDB(**city.dict())
+def get_all_cities(db: Session, ):
+    return db.query(CityDB).all()
+
+
+def create_new_city(db: Session, city: CityCreate):
+    db_city = CityDB(
+        name=city.name,
+        additional_info=city.additional_info,
+    )
     db.add(db_city)
     db.commit()
     db.refresh(db_city)
     return db_city
-
-
-@app.get("/cities/")
-def get_cities(db: Session = Depends(get_db)):
-    return db.query(CityDB).all()
-
-
-@app.get("/cities/{city_id}", response_model=City)
-def get_city(city_id: int, db: Session = Depends(get_db)):
-    city = db.query(CityDB).filter(CityDB.id == city_id).first()
-    if city is None:
-        raise HTTPException(status_code=404, detail="City not found")
-    return city
-
-
-@app.put("/cities/{city_id}", response_model=City)
-def update_city(city_id: int, city: City, db: Session = Depends(get_db)):
-    db_city = db.query(CityDB).filter(CityDB.id == city_id).first()
-    if db_city is None:
-        raise HTTPException(status_code=404, detail="City not found")
-    for key, value in city.dict().items():
-        setattr(db_city, key, value)
-    db.commit()
-    db.refresh(db_city)
-    return db_city
-
-
-@app.delete("/cities/{city_id}")
-def delete_city(city_id: int, db: Session = Depends(get_db)):
-    city = db.query(CityDB).filter(CityDB.id == city_id).first()
-    if city is None:
-        raise HTTPException(status_code=404, detail="City not found")
-    db.delete(city)
-    db.commit()
-    return {"message": "City deleted successfully"}
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from city.crud import get_all_cities, create_new_city
+from city.schemas import City, CityCreate
+from dependencies import get_db
+
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"Hello": "World"}
+
+@app.get("/cities/", response_model=List[City])
+def get_cities(db: Session = Depends(get_db)):
+    return get_all_cities(db)
+
+
+@app.post("/cities/")
+def create_city(city: CityCreate, db: Session = Depends(get_db)):
+    return create_new_city(db=db, city=city)
+
